ceb10cc979aa62bdefd538baebb383fe94376d03
Samantha Copland <sam.copland1992@gmail.com>
2020-05-11 15:37:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ function happyHolidays () {return "Happy holidays!"}
 function happyHolidaysTo (name) {return "Happy holidays, " + `${name}!`}
 function happyCustomHolidayTo (holiday, name)
  {return "Happy " + `${holiday}, ` + `${name}!`}
+function holidayCountdown (holiday, days)
+{return "It's " + `${days}` + "until " + `${holday}!`}
