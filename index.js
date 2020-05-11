ca790c8100d227279a1fccf7e0299398a5507d38
Samantha Copland <sam.copland1992@gmail.com>
2020-05-11 15:34:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // write your code below!
 function happyHolidays () {return "Happy holidays!"}
 function happyHolidaysTo (name) {return "Happy holidays, " + `${name}!`}
-function happyCustomHolidayTo (holiday, name) {return "Happy " + `${holiday}, ` + `${name}!`}
+function happyCustomHolidayTo (holiday, name)
+ {return "Happy " + `${holiday}, ` + `${name}!`}
